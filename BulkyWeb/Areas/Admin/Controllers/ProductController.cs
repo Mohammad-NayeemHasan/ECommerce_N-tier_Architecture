decdb3c4b80a45d6e709ffef59bbdbf158365859
NayeemHossen <mdnayeemhasan705@gmail.com>
2025-10-26 18:30:05 +0600

    ec

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -85,30 +85,30 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (ProductFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(ProductFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product Product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(Product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product Updated Successfully";
-            }
-            return RedirectToAction("Index");
-        }
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+        //    if (ProductFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(ProductFromDb);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(Product Product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Product.Update(Product);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Product Updated Successfully";
+        //    }
+        //    return RedirectToAction("Index");
+        //}
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
